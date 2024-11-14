0f3ebf1ae98c8ee0a64278442cd6c77c36d9e237
martinromario55 <martinromario55@gmail.com>
2024-11-14 23:14:08 +0300

    Dashboard page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,33 +1,36 @@
-import { Footer } from '@/components/footer'
-import { Header } from '@/components/header'
-import { RenderMounted } from '@/components/render-mounted'
-import { ADMIN } from '@/constants/constants'
-import { createClient } from '@/supabase/server'
-import { redirect } from 'next/navigation'
-import { ReactNode } from 'react'
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { RenderMounted } from "@/components/render-mounted";
+import { ADMIN } from "@/constants/constants";
+import { createClient } from "@/supabase/server";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+import { revalidatePath } from "next/cache";
+
+// revalidatePath("/", "layout");
 
 export default async function AdminLayout({
   children,
 }: Readonly<{
-  children: ReactNode
+  children: ReactNode;
 }>) {
-  const supabase = createClient()
+  const supabase = createClient();
 
-  const { data: authData } = await supabase.auth.getUser()
+  const { data: authData } = await supabase.auth.getUser();
 
   if (authData?.user) {
     const { data, error } = await supabase
-      .from('users')
-      .select('*')
-      .eq('id', authData.user.id)
-      .single()
+      .from("users")
+      .select("*")
+      .eq("id", authData.user.id)
+      .single();
 
     if (error || !data) {
-      console.log('Error fetching user data', error)
-      return
+      console.log("Error fetching user data", error);
+      return;
     }
 
-    if (data.type === ADMIN) return redirect('/')
+    if (data.type === ADMIN) return redirect("/");
   }
 
   return (
@@ -36,5 +39,5 @@ export default async function AdminLayout({
       <main className="min-h-[calc(100svh-128px)] py-3">{children}</main>
       <Footer />
     </RenderMounted>
-  )
+  );
 }
